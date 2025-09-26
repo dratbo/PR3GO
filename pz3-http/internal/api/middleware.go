fdcee902d0b90be6559505a43222df962c7f2d7b
dratbo <dratbo03@mail.ru>
2025-09-26 17:17:39 +0300

    Update middleware.go

--- pz3-http/internal/api/middleware.go
+++ pz3-http/internal/api/middleware.go
@@ -6,21 +6,21 @@ import (
 	"time"
 )
 
-type statusRecorder struct {
+type statusRecorder struct {  // statusRecorder - обертка для ResponseWriter для захвата статуса
 	http.ResponseWriter
 	status int
 }
 
 func (sr *statusRecorder) WriteHeader(code int) {
-	sr.status = code
+	sr.status = code  // Запоминаем статус
 	sr.ResponseWriter.WriteHeader(code)
 }
 
-func Logging(next http.Handler) http.Handler {
+func Logging(next http.Handler) http.Handler {  // Logging - middleware для логирования запросов
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		rec := &statusRecorder{ResponseWriter: w, status: 200}
-		next.ServeHTTP(rec, r)
-		log.Printf("%s %s %d %v", r.Method, r.URL.Path, rec.status, time.Since(start))
+		next.ServeHTTP(rec, r)  // Передаем управление следующему обработчику
+		log.Printf("%s %s %d %v", r.Method, r.URL.Path, rec.status, time.Since(start))  // Логируем после обработки запроса
 	})
 }
